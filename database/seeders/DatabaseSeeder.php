afec00ae53b294d9dd0d6ae5a1a2ecab62cdebda
kurnovian115 <kurazinovian115@gmail.com>
2025-11-13 19:10:32 +0700

    Penambahan role Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,49 +28,8 @@ class DatabaseSeeder extends Seeder
             UnitSeeder::class,
             JenisLayananSeeder::class,
             KategoriPengaduanSeeder::class,
+            UserSeeder::class,
         ]);
-
-        // Buat user Admin Kanwil pertama jika belum ada
-    $roleId = Role::where('name', 'admin_kanwil')->value('id');
-    // $unitId = Unit::where('type', 'kanwil')->value('id');
-
-    User::firstOrCreate(
-        ['nip' => '197912312005011001'],
-        [
-            'name' => 'Admin Kanwil',
-            'email' => 'admin.kanwil@example.com',
-            'password' => Hash::make('password'),
-            'role_id' => $roleId,
-            'unit_id' => '1',
-            'is_active' => true,
-            'layanan_id' => null,
-        ]        
-    );
-        $roleId = Role::where('name', 'admin_upt')->value('id');
-     User::firstOrCreate(
-        ['nip' => '197912312005011002'],
-        [
-            'name' => 'Admin UPT',
-            'email' => 'admin.upt@example.com',
-            'password' => Hash::make('password'),
-            'role_id' => $roleId,
-            'unit_id' => '1',
-            'is_active' => true,
-            'layanan_id' => null,
-        ]
-     );
-
-    // Pengaduan::factory()->count(10)->create();
-
-    // // minimal data referensi
-    // User::factory()->count(2)->create();
-    // Unit::factory()->count(1)->create();
-
-    // JenisLayanan::factory()->count(5)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+      
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $roleId = Role::where('name', 'admin_kanwil')->value('id');
+        $users = [
+    ];
+        User::insert([
+            ['nip' => '197912312005011001',
+                'name' => 'Admin Kanwil',
+                'email' => 'admin.kanwil@example.com',
+                'password' => Hash::make('password'),
+                'role_id' => $roleId,
+                'unit_id' => null,
+                'is_active' => true,
+                'layanan_id' => null,
+                'created_at' => now(),
+            ],
+            ['nip' => '197109241994031001',
+                'name' => 'Admin Kanwil',
+                'email' => 'admin.kanwil@example.com',
+                'password' => Hash::make('wibowo'),
+                'role_id' => $roleId,
+                'unit_id' => null,
+                'is_active' => true,
+                'layanan_id' => null,
+                'created_at' => now(),
+            ],
+            // admin UPT
+            ['nip' => '197912312005011002',
+                'name' => 'Admin UPT',
+                'email' => 'admin.upt@example.com',
+                'password' => Hash::make('password'),
+                'role_id' => $roleId,
+                'unit_id' => 1,
+                'is_active' => true,
+                'layanan_id' => null,
+                'created_at' => now(),
+            ],
+                
+        ]);
+
+      
+    }
+}
