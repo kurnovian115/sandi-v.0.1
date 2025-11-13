4d062355511c4099aef62d66d146b152b6b4dc2b
kurnovian115 <kurazinovian115@gmail.com>
2025-11-13 09:03:54 +0700

    commit

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Sandi Jabar - Pengaduan</title>
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
